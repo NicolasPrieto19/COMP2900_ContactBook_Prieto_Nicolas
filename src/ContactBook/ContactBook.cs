6a3099d03a112d57747612c12055f27ead667b66
NicolasPrieto <NPrieto5685@interbayamon.edu>
2026-04-30 15:41:38 -0400

    Anadiendo pantalla de salida

--- src/ContactBook/ContactBook.cs
+++ src/ContactBook/ContactBook.cs
@@ -194,6 +194,8 @@ public class ContactBook
 
     private void ShowExitScreen()
     {
+        Console.Clear();
+        Console.WriteLine("Thank you for using Nicola's Contact Book. Goodbye!");
 
     }
 
